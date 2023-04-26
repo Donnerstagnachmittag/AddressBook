daee5f60be508a4644ff462df6aa3f24acfaddd5
Samuel Wehrli <63265587+samuelwehrli@users.noreply.github.com>
2023-04-26 16:27:41 +0200

    Including User Login

--- addressbook_app.py
+++ addressbook_app.py
@@ -1,14 +1,39 @@
 import streamlit as st
 import pandas as pd
 from geocode import get_coordinates
-from jsonbin import load_data, save_data
+from jsonbin import load_key, save_key
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
 
-# Key und bin id laden
+# -------- load secrets for jsonbin.io --------
 jsonbin_secrets = st.secrets["jsonbin"]
+api_key = jsonbin_secrets["api_key"]
+bin_id = jsonbin_secrets["bin_id"]
 
-# Titel der App
-st.title("Adressbuch App - Cloud Version")
+# -------- user login --------
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+)
+
+fullname, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status == True:   # login successful
+    authenticator.logout('Logout', 'main')   # show logout button
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+    st.stop()
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+    st.stop()
+
+# --------- sidebar ---------
 # Sidebar zum Hinzufügen neuer Adressen
 st.sidebar.header("Neue Adresse hinzufügen")
 name = st.sidebar.text_input("Name")
@@ -16,6 +41,11 @@ street = st.sidebar.text_input("Straße")
 city = st.sidebar.text_input("Stadt")
 submit = st.sidebar.button("Adresse hinzufügen")
 delete = st.sidebar.button("Lezter Eintrag löschen")
+
+# -------- main app --------
+
+# Titel der App
+st.title(f"Adressbuch von {fullname}")
 
 # Funktion zum Hinzufügen einer neuen Adresse zur Adressliste
 if submit:
@@ -29,22 +59,23 @@ if submit:
         "latitude": latitude,
         "longitude": longitude
     }
-    address_list = load_data(jsonbin_secrets["api_key"], jsonbin_secrets["bin_id"])
+    address_list = load_key(api_key, bin_id, username)
     address_list.append(new_address)
-    res = save_data(jsonbin_secrets["api_key"], jsonbin_secrets["bin_id"], address_list)
+    res = save_key(api_key, bin_id, username, address_list)
     if 'message' in res:
         st.error(res['message'])
 
+# Löschen der letzten Adresse
 if delete:
     # delete last entry
-    address_list = load_data(jsonbin_secrets["api_key"], jsonbin_secrets["bin_id"])
+    address_list = load_key(api_key, bin_id, username)
     address_list.pop()
-    res = save_data(jsonbin_secrets["api_key"], jsonbin_secrets["bin_id"], address_list)
+    res = save_key(api_key, bin_id, username, address_list)
     if 'message' in res:
         st.error(res['message'])
 
-# Darstellung der Adressliste als Tabelle
-address_list = load_data(jsonbin_secrets["api_key"], jsonbin_secrets["bin_id"])
+# Darstellung der Adressliste als Tabelle mit Karte
+address_list = load_key(api_key, bin_id, username)
 df = pd.DataFrame(address_list)
 st.table(df)
-st.map(df)
+st.map(df.dropna())
